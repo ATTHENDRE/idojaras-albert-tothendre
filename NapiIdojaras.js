2d6066d2866fdea8b64dc3e8c7a18c132d098f8b
Petrik Diak <petrik@diak.hu>
2026-02-13 13:25:04 +0100

    0-6 kozotti "Napok" megcsinalva illetve egy peldany felveve

--- NapiIdojaras.js
+++ NapiIdojaras.js
@@ -4,6 +4,30 @@ class NapiIdojaras{
         this.MaxHomer = MaxHomer;
         this.MinHomer = MinHomer;
         this.idojarasSzoveg = idojarasSzoveg;
+
+        if(this.hetNapja == 0){
+            this.hetNapja = "Vasárnap";
+        }
+        else if(this.hetNapja == 1){
+            this.hetNapja = "Hétfő";
+        }
+        else if(this.hetNapja == 2){
+            this.hetNapja = "Kedd";
+        }
+        else if(this.hetNapja == 3){
+            this.hetNapja = "Szerda";
+        }
+        else if(this.hetNapja == 4){
+            this.hetNapja = "Csütörtök";
+        }
+        else if(this.hetNapja == 5){
+            this.hetNapja = "Péntek";
+        }
+        else if(this.hetNapja == 6){
+            this.hetNapja = "Szombat";
+        }
+
+
     }
 
     toString(){
---
+++ index.js
@@ -0,0 +1,5 @@
+import NapiIdojaras from "./NapiIdojaras.js";
+
+const UjIdojaras = new NapiIdojaras(2, 30, 20, "Napos idő");
+
+console.log(UjIdojaras.toString());
